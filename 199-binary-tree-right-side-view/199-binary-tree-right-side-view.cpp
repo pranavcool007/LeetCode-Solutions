c66cf2540992ee21d08cef1009ae23759973daab
pranavcool007 <82723340+pranavcool007@users.noreply.github.com>
2022-02-09 23:49:30 +0530

    Time: 3 ms (76.68%), Space: 11.9 MB (89.60%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
@@ -11,34 +11,53 @@
  */
 class Solution {
 public:
-    vector<int> rightSideView(TreeNode* root) {
-        vector<int> ans;
-        if(root == NULL) return ans;
-        queue<pair<TreeNode*,int>> q;
-        map<int,int> mpp;
-        q.push({root,0});
-        
-        while(!q.empty()){
-            auto it = q.front();
-            q.pop();
-            TreeNode* node = it.first;
-            int level = it.second;
-            
-            if(mpp.find(level) == mpp.end()){
-                mpp[level] = node->val;
-            }
-            
-            if(node->right){
-                q.push({node->right,level+1});
-            }
-            if(node->left){
-                q.push({node->left,level+1});
-            }
+    void rightside(TreeNode* root,int level,vector<int> &ans){
+        if(root == NULL){
+            return;
         }
-        for(auto temp: mpp){
-            ans.push_back(temp.second);
+     
+        if(level == ans.size()){
+            ans.push_back(root->val);
         }
-        return ans;
+        rightside(root->right,level+1,ans);
+        rightside(root->left,level+1,ans);
         
     }
+
+    vector<int> rightSideView(TreeNode* root) {
+//         vector<int> ans;
+//         if(root == NULL) return ans;
+//         queue<pair<TreeNode*,int>> q;
+//         map<int,int> mpp;
+//         q.push({root,0});
+        
+//         while(!q.empty()){
+//             auto it = q.front();
+//             q.pop();
+//             TreeNode* node = it.first;
+//             int level = it.second;
+            
+//             if(mpp.find(level) == mpp.end()){
+//                 mpp[level] = node->val;
+//             }
+            
+//             if(node->right){
+//                 q.push({node->right,level+1});
+//             }
+//             if(node->left){
+//                 q.push({node->left,level+1});
+//             }
+//         }
+//         for(auto temp: mpp){
+//             ans.push_back(temp.second);
+//         }
+//         return ans;
+        
+    int level = 0;
+    vector<int> ans;
+    rightside(root,level,ans);
+    return ans;
+    }
+        
+
 };
