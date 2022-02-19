fd7d7958853ce82dc351e9104893ccc885d6d0c6
pranavcool007 <82723340+pranavcool007@users.noreply.github.com>
2022-02-19 23:38:46 +0530

    Time: 105 ms (33.03%), Space: 55.5 MB (64.90%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -1,35 +1,11 @@
 class Solution {
 public:
-//     int longestOnes(vector<int>& nums, int k) {
-//         int count = 0;
-//         int ans = 0;
-//         int i = -1;
-//         int j = -1;
-//         while(i<nums.size()-1){
-//             ++i;
-//             if(nums[i] == 0){
-//                 count++;
-//             }
-            
-//             while(count > k){
-//                 ++j;
-//                 if(nums[j] == 0){
-//                     --count;
-//                 }
-//             }
-            
-//             int len = i-j;
-//             ans = max(ans,len);
-            
-//         }
-//         return ans;
-//     }
     int longestOnes(vector<int>& nums, int k) {
         int count = 0;
         int ans = 0;
         int i = -1;
         int j = -1;
-        while(i<(int)nums.size()-1){
+        while(i<int(nums.size()-1)){
             ++i;
             if(nums[i] == 0){
                 count++;
@@ -47,5 +23,29 @@ public:
             
         }
         return ans;
-}
+    }
+//     int longestOnes(vector<int>& nums, int k) {
+//         int count = 0;
+//         int ans = 0;
+//         int i = -1;
+//         int j = -1;
+//         while(i<(int)nums.size()-1){
+//             ++i;
+//             if(nums[i] == 0){
+//                 count++;
+//             }
+            
+//             while(count > k){
+//                 ++j;
+//                 if(nums[j] == 0){
+//                     --count;
+//                 }
+//             }
+            
+//             int len = i-j;
+//             ans = max(ans,len);
+            
+//         }
+//         return ans;
+// }
 };
