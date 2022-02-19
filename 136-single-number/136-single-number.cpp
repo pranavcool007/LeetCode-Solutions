fffcba1b2d0610bfa719785bc48dcfccef320c20
pranavcool007 <82723340+pranavcool007@users.noreply.github.com>
2022-02-19 14:11:04 +0530

    Time: 32 ms (36.74%), Space: 20.1 MB (9.49%) - LeetHub

---
+++ 136-single-number/136-single-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans;
+        unordered_map<int,int> mpp;
+        
+        for(int i=0; i<nums.size(); ++i){
+            ++mpp[nums[i]];
+        }
+        
+        for(auto i: mpp){
+            if(i.second == 1){
+                ans = i.first;
+            }
+        }
+        return ans;
+    }
+};
