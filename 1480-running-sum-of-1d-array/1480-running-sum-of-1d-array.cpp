584e4b4aeeb14131b6b4b541d1e1d3268be52374
pranavcool007 <82723340+pranavcool007@users.noreply.github.com>
2022-02-19 13:09:26 +0530

    Time: 7 ms (26.65%), Space: 8.5 MB (75.25%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
@@ -5,7 +5,7 @@ public:
         vector<int> ans;
         
         for(int i=0; i<nums.size(); ++i){
-            sum += nums[i];
+            sum += nums[i];             // keep maintaining the sum till each idx
             ans.push_back(sum);
         }
         return ans;
