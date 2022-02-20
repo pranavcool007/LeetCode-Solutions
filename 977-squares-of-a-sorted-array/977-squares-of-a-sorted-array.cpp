da10b1118a8159771be87289fcfadbb13c27bff2
pranavcool007 <82723340+pranavcool007@users.noreply.github.com>
2022-02-20 13:23:50 +0530

    Time: 54 ms (38.47%), Space: 25.8 MB (95.00%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
@@ -4,11 +4,12 @@ public:
         int n = nums.size();
         vector<int> ans(n);
         
-        int i = 0;
-        int j = n-1;
-        int k = n-1;
+        int i = 0;        // i to srting of nums
+        int j = n-1;     // j to end of nums
+        int k = n-1;     // k point to the ans vector
         while(k>=0){
-            if(abs(nums[i])>abs(nums[j])){
+            if(abs(nums[i])>abs(nums[j])){  // if no is greater so will be squares
+                                            // also the greatest ele will be at extremes
                 ans[k] = nums[i]*nums[i];
                 ++i;
                 --k;
