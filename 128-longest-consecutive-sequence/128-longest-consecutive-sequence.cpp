b61c03dfc097b5795e2fc2ba76d84d40506fe2c8
pranavcool007 <82723340+pranavcool007@users.noreply.github.com>
2022-02-20 12:51:20 +0530

    Time: 834 ms (15.71%), Space: 31 MB (45.66%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -3,16 +3,17 @@ public:
     int longestConsecutive(vector<int>& nums) {
         unordered_map<int,bool> mpp;
         
-        for(auto i : nums){
+        for(auto i : nums){   // every no says seq starts from it
             mpp[i] = true;
         }
-        for(auto i: nums){
+        for(auto i: nums){    // check incompatible strt points
             if(mpp.find(i-1) != mpp.end()){
                 mpp[i] = false;
             }
         }
         int len = 0;
-        int sp;
+        int sp;              // only check for the srt points, and update
+                             // max len and srt points
         for(auto i: nums) {
             if(mpp[i] == true){
                 int tlen = 1;
