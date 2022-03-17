d83b94e94246a6ccb2477bfb3a77785077fae8e9
pranavcool007 <82723340+pranavcool007@users.noreply.github.com>
2022-03-17 19:14:17 +0530

    Time: 12 ms (83.84%), Space: 10 MB (55.46%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.cpp
+++ 16-3sum-closest/16-3sum-closest.cpp
@@ -7,7 +7,7 @@ public:
         int ans = nums[0]+nums[1]+nums[2];
         
         for(int i=0; i<nums.size(); ++i){
-            if(i>0 and nums[i] == nums[i-1]) continue;
+            //if(i>0 and nums[i] == nums[i-1]) continue;
             int j = i+1;
             int k = nums.size()-1;
             while(j<k){
