de0f5ff9a6ddba68bbdaaaf7ee8ee71c7f7e407e
pranavcool007 <82723340+pranavcool007@users.noreply.github.com>
2022-03-18 20:07:52 +0530

    Time: 7 ms (90.76%), Space: 7.5 MB (76.11%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
@@ -12,18 +12,18 @@ public:
         ListNode* slow = head;
         ListNode* fast = head;
         while(true){
-            if(fast == NULL or fast->next == NULL){
-                return NULL;
+            if(fast == NULL or fast->next == NULL){  // if there is no cycle in the list
+                return NULL;                         // also a check for fast->next->next
             }
             slow = slow->next;
-            fast = fast->next->next;
+            fast = fast->next->next;    
             if(slow == fast){
                 break;
             }
 
         }
         ListNode* slow2 = head;
-        while(slow != slow2){
+        while(slow != slow2){        // if slow2 and slow already smae
             slow = slow->next;
             slow2 = slow2->next;
         }
