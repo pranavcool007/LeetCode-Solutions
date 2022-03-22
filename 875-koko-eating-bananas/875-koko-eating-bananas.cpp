f2d7d76e4bd8f64991caf29929036d9161228919
pranavcool007 <82723340+pranavcool007@users.noreply.github.com>
2022-03-22 17:30:35 +0530

    Time: 40 ms (93.92%), Space: 19 MB (8.33%) - LeetHub

--- 875-koko-eating-bananas/875-koko-eating-bananas.cpp
+++ 875-koko-eating-bananas/875-koko-eating-bananas.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
+    bool feasible(int speed,vector<int> &piles,int Hours){
+        int hours = 0;
+        for(int pile:piles){
+            hours += ceil((1.0*pile)/speed);
+        }
+        if(hours > Hours){
+            return false;
+        }
+        return true;
+    }
     int minEatingSpeed(vector<int>& piles, int h) {
-        // range of eating speed(banana per hr)
-        int left = 1;
-        int right = *max_element(piles.begin(),piles.end()); 
-        
-        // min_eating_speed
-        int ans = right;
-        
-        while(left <= right){
-            int mid = left + (right-left)/2;
-            int time = 0;
-            
-            for(int i: piles){
-                time += ceil(1.0*i/mid);
-            }
-            if(time <= h){
-                ans = min(ans,mid);
-                right = mid-1;
+        int start = 1;
+        int end = *max_element(piles.begin(),piles.end());
+        while(start <= end){
+            int mid = start + (end-start)/2;
+            if(feasible(mid,piles,h)){
+                end = mid-1;
             }
             else{
-                left = mid+1;
+                start = mid+1;
             }
         }
-        return ans;
+        return start;
     }
 };
