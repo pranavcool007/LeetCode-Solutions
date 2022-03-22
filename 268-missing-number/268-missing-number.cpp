36cfbdc018516c21d3fb68881987c6d02bb32be3
pranavcool007 <82723340+pranavcool007@users.noreply.github.com>
2022-03-22 23:48:56 +0530

    Time: 32 ms (38.47%), Space: 18 MB (65.19%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
+//         {
+//         unordered_map<int,int> mpp;
+//         int ans;
+        
+//         for(int i=0; i<nums.size(); ++i){
+//             ++mpp[nums[i]];
+//         }
+        
+//         for(int i =0; i<=nums.size(); ++i){
+//             if(mpp.find(i) == mpp.end()){
+//                 ans = i;
+//                 break;
+//             }
+//         }
+//         return ans;
+//     }
+        // O(1) Space and O(n) Time
         int len = nums.size();
         int sum1 = 0,sum2 = 0;
         for(int i:nums){
