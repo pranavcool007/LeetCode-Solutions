2159c376f5d879ff4d86328276d60b64e913c7b4
pranavcool007 <82723340+pranavcool007@users.noreply.github.com>
2022-04-02 15:00:20 +0530

    Time: 70 ms (48.86%), Space: 27.2 MB (13.34%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
@@ -1,11 +1,17 @@
 class Solution {
 public:
+    // only one char removal is allowed
+    /* eg aaaz in this i -> a and j -> z, so either we can remove
+    a and check if aaz is palindrome or not or we can remove z and
+    check if aa if palindrome or not. if any of the above cases are 
+    true then return true or return false
+    */
     
     bool validPalindrome(string s) {
         int j = s.length()-1;
         int i = 0;
         while(i < j){
-            if(s[i] == s[j]){
+            if(s[i] == s[j]){       
                 ++i;
                 --j;
             }
