a863efefbc54e8594928676091d327c7ce276a7c
pranavcool007 <82723340+pranavcool007@users.noreply.github.com>
2022-04-04 20:24:00 +0530

    Time: 21 ms (57.30%), Space: 16.9 MB (99.34%) - LeetHub

--- 57-insert-interval/57-insert-interval.cpp
+++ 57-insert-interval/57-insert-interval.cpp
@@ -5,11 +5,12 @@ public:
         int interval = 0;
         int start = 0;
         int end = 1;
+        // merging intervals that are less than new interval
         while(interval < intervals.size() and intervals[interval][end] < newInterval[start]){
             ans.push_back(intervals[interval]);
             ++interval;
         }
-        
+        // merging intervals that are overlapping with the new Interval
         while(interval < intervals.size() and intervals[interval][start] <= newInterval[end]){
             newInterval[start] = min(newInterval[start],intervals[interval][start]);
             newInterval[end] = max(newInterval[end],intervals[interval][end]);
@@ -17,6 +18,7 @@ public:
         }
         ans.push_back(newInterval);
         
+        // merging intervals that are greater than the new Interval
         while(interval < intervals.size() and intervals[interval][start] > newInterval[end]){
             ans.push_back(intervals[interval]);
             ++interval;
