d4ab299327e1e5ca150e210c064455ccf8822914
pranavcool007 <82723340+pranavcool007@users.noreply.github.com>
2022-04-06 00:46:47 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (90.90%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
+    //Game of Execution - Josephus Problem 
     int findTheWinner(int n, int k) {
         if(n == 1){
             return 1;
         }
         int x = findTheWinner(n-1,k);
-        int y = (x+k-1)%(n)+1;
+        int y = (x+k-1)%(n)+1;          // 1 based indexing
+                                        // 0 based indexing  (x+k)%n
         return y;
     }
 };
