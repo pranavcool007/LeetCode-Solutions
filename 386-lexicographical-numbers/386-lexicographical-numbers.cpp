178346876d9a003a64b4a20781624f075e855dbb
pranavcool007 <82723340+pranavcool007@users.noreply.github.com>
2022-04-06 01:18:13 +0530

    Time: 23 ms (33.17%), Space: 12.1 MB (29.35%) - LeetHub

--- 386-lexicographical-numbers/386-lexicographical-numbers.cpp
+++ 386-lexicographical-numbers/386-lexicographical-numbers.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<int> ans;
     void dfs(int i, int n){
-        if(i > n) return;
+        if(i > n) return;   // jb no given range se bada ho jaye us no se return kr jayo
         
         ans.push_back(i);
         for(int j=0 ; j<=9; ++j){
