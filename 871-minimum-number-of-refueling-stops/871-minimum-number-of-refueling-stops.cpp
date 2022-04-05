5237fe29c639c72c7f793acb41469b4a77032571
pranavcool007 <82723340+pranavcool007@users.noreply.github.com>
2022-04-05 13:26:56 +0530

    Time: 20 ms (98.82%), Space: 16.3 MB (53.48%) - LeetHub

--- 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.cpp
+++ 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.cpp
@@ -5,15 +5,25 @@ public:
         int count = 0;
         priority_queue<int> maxh;
         int max_reach = startFuel;
-    
-        while(max_reach < target)
+        
+/* if target is reached then no need to take one more station to fuel the car, tar = 1
+   max_reach = 1 and stations (any) then why to refuel. */
+        while(max_reach < target)   
         {
+         
+/* this saves all the fuel stations till the max_reach point and to calculate the least 
+   no of fuel stations we need max fuel from a station(greedy), so max heap is used to 
+   find which station till the point gives max fuel */
             
             while(index < stations.size() and stations[index][0] <= max_reach)
             {
                 maxh.push(stations[index][1]);
                 ++index;
             }
+            
+/* if no fuel stations till the max_reach point it means we cannot reach the target so
+   return -1  */
+            
             if(maxh.empty()) return -1;
             
             int max_fuel = maxh.top();
