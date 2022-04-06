62999e3bb1eba3f413f8a7baa85d696d62303f25
pranavcool007 <82723340+pranavcool007@users.noreply.github.com>
2022-04-07 01:40:37 +0530

    Time: 25 ms (74.91%), Space: 10.6 MB (62.54%) - LeetHub

--- 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
@@ -1,14 +1,39 @@
 class Solution {
 public:
     int threeSumMulti(vector<int>& arr, int target) {
-        int n = arr.size(), mod = 1e9+7, ans = 0;
-        unordered_map<int, int> m;
-        
-        for(int i=0; i<n; i++) {
-            ans = (ans + m[target - arr[i]]) % mod;
-            
-            for(int j=0; j<i; j++) m[arr[i] + arr[j]]++;
+        long ans = 0;
+        unordered_map<int,long> mpp;
+        for(auto i : arr){
+            mpp[i]++;
         }
-        return ans;
+        
+        for(auto i1 : mpp)
+        {
+            for(auto i2 : mpp)
+            {
+                int i = i1.first;
+                int j = i2.first;
+                int k = target -i-j;
+                if(mpp.find(k) != mpp.end())
+                {
+                    if(i==j and j==k){
+                        long n = mpp[i];
+                        ans += (n*(n-1)*(n-2))/6;
+                    }
+                    else if(i==j and j!=k){
+                        int n1 = mpp[i];
+                        int n2 = mpp[k];
+                        ans += n2*(n1)*(n1-1)/2;
+                    }
+                    else if(i<j and j<k){
+                        int n1 = mpp[i];
+                        int n2 = mpp[j];
+                        int n3 = mpp[k];
+                        ans += n1*n2*n3;
+                    }
+                }
+            }
+        }
+        return ans % 1000000007;
     }
 };
