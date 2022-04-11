5425a9329572ace52ca3635e2c537f2737b4654e
pranavcool007 <82723340+pranavcool007@users.noreply.github.com>
2022-04-11 21:25:36 +0530

    Time: 20 ms (96.30%), Space: 14 MB (73.22%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.cpp
+++ 1260-shift-2d-grid/1260-shift-2d-grid.cpp
@@ -9,8 +9,8 @@ public:
             for(int j=0; j<n; ++j){
                 int arr_val = i*n + j;      // 2d to 1d convert
                 int shifted = (arr_val + k) % (m*n);  // shifting by k on 1d array
-                int new_row = shifted/n;    // 2d row of 1d array
-                int new_col = shifted%n;    // 2d col of 1d array
+                int new_row = shifted/n;    // 2d row of 1d idx
+                int new_col = shifted%n;    // 2d col of 1d idx
                 ans[new_row][new_col] = grid[i][j];
             }
         }
