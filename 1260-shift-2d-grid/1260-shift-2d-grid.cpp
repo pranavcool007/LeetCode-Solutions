2f2bdd0730269ebae393a4fd9617661c4daef8fd
pranavcool007 <82723340+pranavcool007@users.noreply.github.com>
2022-04-12 00:03:57 +0530

    Time: 49 ms (45.01%), Space: 14 MB (73.22%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.cpp
+++ 1260-shift-2d-grid/1260-shift-2d-grid.cpp
@@ -4,6 +4,7 @@ public:
         int m = grid.size();
         int n = grid[0].size();
         vector<vector<int>> ans(m, vector<int> (n,0));
+        k = k%(m*n);
         
         for(int i=0; i<m; ++i){
             for(int j=0; j<n; ++j){
