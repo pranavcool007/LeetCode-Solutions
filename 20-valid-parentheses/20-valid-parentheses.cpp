df94b73df7cff312d6d97b5d8806329f1c440842
pranavcool007 <82723340+pranavcool007@users.noreply.github.com>
2022-04-14 00:56:27 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (10.02%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -1,5 +1,12 @@
 class Solution {
 public:
+/*  best way is to make mappings of opening and closing brackets, opening as +ve no and same 
+    no as -ve in closing.
+    then we have to check 2 things for valid parenthesis:
+    1. at the end of the string stk shd be empty.
+    2. at the time of popping check that the stack shd be >0 in order to pop something from 
+       it. eg to handle cases like (, (( , ([{ .
+*/
     unordered_map<char,int> mpp{{'[',1}, {'{',2},{'(',3},{']',-1}, {'}',-2},{')',-3}};
     bool isValid(string s) {
         stack<int> stk;
