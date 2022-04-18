62b8dd90620b936cbcd44cb5fd9f252198feedcf
pranavcool007 <82723340+pranavcool007@users.noreply.github.com>
2022-04-18 23:05:22 +0530

    Time: 308 ms (77.99%), Space: 157.6 MB (33.73%) - LeetHub

---
+++ 1609-even-odd-tree/1609-even-odd-tree.cpp
@@ -0,0 +1,62 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isEvenOddTree(TreeNode* root) {
+        queue<TreeNode*> q;
+        q.push(root);
+        int level_no = -1;
+        
+        while(q.size() > 0){
+            int count = q.size();
+            ++level_no;
+            vector<int> level;
+            
+            for(int i=0; i<count; ++i){
+                TreeNode* node = q.front();
+                q.pop();
+                if(level_no % 2 == 0){
+                    if(node->val % 2 == 0){
+                        return false;
+                    }
+                }else{
+                    if(node->val % 2 == 1){
+                        return false;
+                    }
+                }
+                level.push_back(node->val);
+                if(node->left){
+                    q.push(node->left);
+                }
+                if(node->right){
+                    q.push(node->right);
+                }
+            }
+            if(level_no % 2 == 0){
+                for(int i=0; i<count-1; ++i){
+                    if(level[i+1] <= level[i]){
+                        return false;
+                    }
+            }
+            }
+            else {
+                for(int i=0; i<count-1; ++i){
+                    if(level[i+1] >= level[i]){
+                        return false;
+                    }
+                }
+            }
+            
+        }
+        return true;
+    }
+};
