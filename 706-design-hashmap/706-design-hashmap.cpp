343aa93ee6cb678b383dc7d91c08d1dde462cb3c
pranavcool007 <82723340+pranavcool007@users.noreply.github.com>
2022-04-23 01:30:41 +0530

    Time: 174 ms (61.76%), Space: 53.1 MB (69.32%) - LeetHub

--- 706-design-hashmap/706-design-hashmap.cpp
+++ 706-design-hashmap/706-design-hashmap.cpp
@@ -1,22 +1,56 @@
 class MyHashMap {
 public:
-    vector<int> mpp;
+    vector<list<pair<int,int>>> mpp;
+    int siz;
     MyHashMap() {
-        int siz = 1e6 + 1;
-        mpp.resize(siz,-1);
+        siz = 1000;
+        mpp.resize(1000);
+    }
+    // extra functions
+    int hash(int key){
+        return key%siz;
+    }
+    list<pair<int,int>> :: iterator search(int key){
+        // as we have {key,value} pair we cannot use find to find the iterator as value we 
+        // dont know
+        int i = hash(key);
+        auto it = mpp[i].begin();      // list ka begin pointer of m[i]
+        while(it != mpp[i].end()){
+            if(it->first == key){
+                return it;
+            }
+            it++;
+        }
+        return it;                     // if key is not found it will return m[i].end();
     }
     
     void put(int key, int value) {
-        mpp[key] = value;
+        int i = hash(key);
+        auto it = search(key);
+        if(it != mpp[i].end()){
+            it->second = value;
+        }
+        else{
+            mpp[i].push_back({key,value});
+        }
+        
     }
     
     int get(int key) {
-        return mpp[key];
+        int i = hash(key);
+        auto it = search(key);
+        if(it != mpp[i].end()){
+            return it->second;
+        }else{
+            return -1;
+        }
     }
     
     void remove(int key) {
-        if(mpp[key] != -1){
-            mpp[key] = -1;
+        int i = hash(key);
+        auto it = search(key);
+        if(it != mpp[i].end()){
+            mpp[i].erase(it);
         }
     }
 };
