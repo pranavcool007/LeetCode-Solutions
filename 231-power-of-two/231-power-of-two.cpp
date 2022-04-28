2074b024df7fab09800b16e1f5b65273a302d2f3
pranavcool007 <82723340+pranavcool007@users.noreply.github.com>
2022-04-28 13:05:45 +0530

    Time: 3 ms (52.39%), Space: 5.9 MB (70.43%) - LeetHub

--- 231-power-of-two/231-power-of-two.cpp
+++ 231-power-of-two/231-power-of-two.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+// this works for +ve nos and all -ves no cannot be a pow of 2 ovio, best is to directly
+// return the results without storing anything to avoid overflow (range given)
     bool isPowerOfTwo(int n) {
         return n > 0 and !(n &(n-1));
     }
