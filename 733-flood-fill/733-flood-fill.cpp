65cb03b17390f40db214cfca79d238d2dda0da5f
pranavcool007 <82723340+pranavcool007@users.noreply.github.com>
2022-04-29 02:48:04 +0530

    Time: 4 ms (96.98%), Space: 13.8 MB (92.06%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
-    void dfs(int i,int j,vector<vector<int>>& image,int k,int newColor){
+    void dfs(int i,int j,vector<vector<int>>& image,int color,int newColor){
         int rows = image.size();
         int cols = image[0].size();
-        if(i<0 || j<0 || i>=rows || j>= cols|| image[i][j] != k){
+        if(i<0 || j<0 || i>=rows || j>= cols|| image[i][j] != color){
             return;
         }
         image[i][j] = newColor;
-        dfs(i+1,j,image,k,newColor);
-        dfs(i-1,j,image,k,newColor);
-        dfs(i,j+1,image,k,newColor);
-        dfs(i,j-1,image,k,newColor);
+        dfs(i+1,j,image,color,newColor);
+        dfs(i-1,j,image,color,newColor);
+        dfs(i,j+1,image,color,newColor);
+        dfs(i,j-1,image,color,newColor);
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
-        int colour =  image[sr][sc];
-        if(colour == newColor) return image;
-        dfs(sr,sc,image,colour,newColor);
+        int color =  image[sr][sc];
+//It would cause StackOverFlow since we would be going back and forth to change the color to NewColor even though it's already NewcColor
+        if(color == newColor) return image;
+        dfs(sr,sc,image,color,newColor);
         return image;
     }
 };
