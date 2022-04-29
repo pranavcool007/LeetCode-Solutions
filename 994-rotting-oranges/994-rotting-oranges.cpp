ab061aaca6a1981a26aaab57102628d57fc5149d
pranavcool007 <82723340+pranavcool007@users.noreply.github.com>
2022-04-30 02:34:27 +0530

    Time: 10 ms (53.76%), Space: 13.1 MB (49.93%) - LeetHub

---
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    bool isValid(int row,int col,int rows,int cols,vector<vector<int>> &grid){
+        if(row<0 || col<0 || row>=rows|| col>=cols|| grid[row][col] != 1){
+            return false;
+        }
+        return true;
+    }
+    int orangesRotting(vector<vector<int>>& grid) {
+        int rows = grid.size();
+        int cols = grid[0].size();
+        int rotten_c = 0;
+        queue<pair<int,int>> q;
+        for(int i=0; i<rows; ++i)
+        {
+            for(int j=0; j<cols; ++j)
+            {
+                if(grid[i][j] == 2){
+                    q.push({i,j});
+                    ++rotten_c;
+                }
+            }
+        }
+        int time = 0;
+        while(!q.empty()){
+            int count = q.size();
+            ++time;
+            for(int i=0; i<count; ++i){
+                int row = q.front().first;
+                int col = q.front().second;
+                q.pop();
+            
+                if(isValid(row+1,col,rows,cols,grid)){
+                    q.push({row+1,col});
+                    grid[row+1][col] = 2;
+                }
+                if(isValid(row-1,col,rows,cols,grid)){
+                    q.push({row-1,col});
+                    grid[row-1][col] = 2;
+                }
+                if(isValid(row,col+1,rows,cols,grid)){
+                    q.push({row,col+1});
+                    grid[row][col+1] = 2;
+                }
+                if(isValid(row,col-1,rows,cols,grid)){
+                    q.push({row,col-1});
+                    grid[row][col-1] = 2;
+                }
+            }
+            
+        }
+        // checking if any fresh orange is present after processing the grid
+        for(int i=0; i<rows; ++i){
+            for(int j=0; j<cols; ++j){
+                if(grid[i][j] == 1){
+                    return -1;
+                }
+            }
+        }
+        if(rotten_c == 0) return 0;
+        return time-1;
+    }
+};
